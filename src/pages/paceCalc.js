b0b040e5d65ef257debb4067af21cd0d092d8903
Chad Heise <chad@heise.org>
2019-01-22 22:57:45 -0800

    Add support links and screenshots for pace-calc app

--- src/pages/paceCalc.js
+++ src/pages/paceCalc.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const SecondPage = () => (
+const SecondPage = ({data}) => (
   <Layout>
     <SEO title="Quick Pace Calc" />
     <Helmet>
@@ -16,8 +17,18 @@ const SecondPage = () => (
     <h1>Quick Pace Calc</h1>
     <p>Quick Pace Calc is an iOS app that allows you to easily convert running times to a variety of formats.</p>
     <p>For example, if you ran 5.3 miles in 46 min and 37 sec, Quick Pace Calc will quickly show you pace in min/mile min/km as well as how long it would take you to compelete common race distances at that pace such as 5k, 10k and more!</p>
+    <h2>Screenshots</h2>
+      <div style={{maxWidth: `400px`}}>
+        <Img aspectRatio={1} fluid={data.screenShot.childImageSharp.fluid}/>
+      </div>
     <h2>Links</h2>
     <ul>
+      <li>
+        <a href="https://github.com/chadheise/pace-calc">Source (GitHub)</a>
+      </li>
+      <li>
+        <a href="https://github.com/chadheise/pace-calc/issues">Support</a>
+      </li>
       <li>
         <Link to="/paceCalc/privacyPolicy">Privacy Policy</Link>
       </li>
@@ -26,3 +37,19 @@ const SecondPage = () => (
 )
 
 export default SecondPage
+
+export const query = graphql`
+{
+  screenShot: file(relativePath: { eq: "pace-calc-screenshot.png"}) {
+    name
+    relativePath
+    extension
+    publicURL
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
